0a7d7a6b12b562baf21940b0da4f95c801195758
Rus <rus.zakirov@triosstudent.com>
2024-10-14 11:32:33 -0400

    Refactored delete_technician page

--- tech_support copy/technician_manager/delete_technician.php
+++ tech_support copy/technician_manager/delete_technician.php
@@ -4,21 +4,29 @@
    error_reporting(E_ALL);
 
    $techID = $_POST['techID'];
-   require_once('../model/database.php');
+   require_once('../model/database_oo.php');
 
    // code to save to MySQL Database goes here
    // validate inputs(checking if none of the inputs are = null)
    if ($techID != false)
    {
-      $query = 'DELETE FROM technicians WHERE techID = :techID';
-      
-      $statement = $db->prepare($query);
-      $statement->bindValue(':techID', $techID);
+      try {
+         $db = Database::getDB();
+         $query = 'DELETE FROM technicians WHERE techID = :techID';
 
-      // processes to the database
-      $statement->execute();
-      // close prepared statement
-      $statement->closeCursor();
+         $statement = $db->prepare($query);
+         $statement->bindValue(':techID', $techID);
+
+         // processes to the database
+         $statement->execute();
+         // close prepared statement
+         $statement->closeCursor();
+
+      }catch(PDOException $e) {
+         $error_message = $e->getMessage();
+         include('../errors/database_error.php');
+         exit();
+      }
    }
 
       // reload index page
